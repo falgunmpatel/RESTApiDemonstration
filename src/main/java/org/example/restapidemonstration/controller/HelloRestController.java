13c3f1604e277cef4a1d85637ebeff834925e5c3
Falgun Patel <falgunmpatel9123@gmail.com>
2025-03-01 15:28:52 +0530

    [Falgun Patel] uc3 - Demonstarte GET with path variables
    Signed-off-by: Falgun Patel <falgunmpatel9123@gmail.com>

--- src/main/java/org/example/restapidemonstration/controller/HelloRestController.java
+++ src/main/java/org/example/restapidemonstration/controller/HelloRestController.java
@@ -1,5 +1,6 @@
 package org.example.restapidemonstration.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,11 @@ public class HelloRestController {
 
   @RequestMapping("/hello/query")
   public String helloQuery(@RequestParam(name = "name", defaultValue = "World") String name) {
+    return "Hello, " + name + "!";
+  }
+
+  @RequestMapping("/hello/path/{name}")
+  public String helloPath(@PathVariable String name) {
     return "Hello, " + name + "!";
   }
 }
