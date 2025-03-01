7cc27e2160cfb76b20c61dbb0b3259a9f986a93f
Falgun Patel <falgunmpatel9123@gmail.com>
2025-03-01 15:40:07 +0530

    [Falgun Patel] uc5 - Demonstarte PUT Api
    Signed-off-by: Falgun Patel <falgunmpatel9123@gmail.com>

--- src/main/java/org/example/restapidemonstration/controller/HelloRestController.java
+++ src/main/java/org/example/restapidemonstration/controller/HelloRestController.java
@@ -5,23 +5,29 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloRestController {
-  @RequestMapping("/hello")
+  @RequestMapping(path = "/hello", method = RequestMethod.GET)
   public String hello() {
     return "Hello, World!";
   }
 
-  @RequestMapping("/hello/query")
+  @RequestMapping(path = "/hello/query", method = RequestMethod.GET)
   public String helloQuery(@RequestParam(name = "name", defaultValue = "World") String name) {
     return "Hello, " + name + "!";
   }
 
-  @RequestMapping("/hello/path/{name}")
+  @RequestMapping(path = "/hello/path/{name}", method = RequestMethod.GET)
   public String helloPath(@PathVariable String name) {
     return "Hello, " + name + "!";
   }
 
-  @RequestMapping("/hello/post")
+  @RequestMapping(path = "/hello/post", method = RequestMethod.POST)
   public String helloPost(@RequestBody User user) {
     return "Hello, " + user.getFirstName() + " " + user.getLastName() + "!";
+  }
+
+  @RequestMapping(path = "/hello/put/{firstName}", method = RequestMethod.PUT)
+  public String helloPut(
+      @PathVariable String firstName, @RequestParam(name = "lastName") String lastName) {
+    return "Hello, " + firstName + " " + lastName + "!";
   }
 }
