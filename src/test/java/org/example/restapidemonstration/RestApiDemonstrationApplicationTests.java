84e6f205b506cd4a47e7684e406c9b4043abb34d
Falgun Patel <falgunmpatel9123@gmail.com>
2025-03-01 14:45:00 +0530

    [Falgun Patel] Initialise Spring Boot Project for REST Demonstration
    Signed-off-by: Falgun Patel <falgunmpatel9123@gmail.com>

---
+++ src/test/java/org/example/restapidemonstration/RestApiDemonstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restapidemonstration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDemonstrationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
