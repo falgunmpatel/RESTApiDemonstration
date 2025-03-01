c9b69c4d7a1f852817cae60bb6116cebb732ff2a
Falgun Patel <falgunmpatel9123@gmail.com>
2025-03-01 15:15:54 +0530

    [Falgun Patel] uc1 - Demonstarte GET with search query
    Signed-off-by: Falgun Patel <falgunmpatel9123@gmail.com>

--- src/main/java/org/example/restapidemonstration/controller/HelloRestController.java
+++ src/main/java/org/example/restapidemonstration/controller/HelloRestController.java
@@ -1,15 +1,18 @@
 package org.example.restapidemonstration.controller;
 
-import org.example.restapidemonstration.model.User;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloRestController {
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
-    }
+  @RequestMapping("/hello")
+  public String hello() {
+    return "Hello, World!";
+  }
+
+  @RequestMapping("/hello/query")
+  public String helloQuery(@RequestParam(name = "name", defaultValue = "World") String name) {
+    return "Hello, " + name + "!";
+  }
 }
